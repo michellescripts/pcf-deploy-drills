005e74805dd9183cbf843aa83760302782b14922
michellescripts <m@michellescripts.com>
2018-02-02 14:02:54 -0700

    Add site for node drill 1

---
+++ node/drills/drill-1/server/data/fixtures.js
@@ -0,0 +1,23 @@
+
+module.exports = {
+  steps: [{
+    id: 1,
+    instructions: 'tbd'
+  },
+  {
+    id: 2,
+    instructions: 'tbd'
+  },
+  {
+    id: 3,
+    instructions: 'tbd'
+  },
+  {
+    id: 4,
+    instructions: 'tbd'
+  },
+  {
+    id: 5,
+    instructions: 'tbd'
+  }
+  ]}
